edf22049fbbadd440fdbe25d3960f1af0e5bc893
anthony <anthony_shum@hotmail.com>
2016-12-16 14:36:13 +0800

    done responsive

---
+++ medplat/Med Platform/Med Platform/navscript.js
@@ -0,0 +1,8 @@
+﻿function myFunction() {
+    var x = document.getElementById("fixednavul");
+    if (x.className === "main") {
+        x.className += " responsive";
+    } else {
+        x.className = "main";
+    }
+}
